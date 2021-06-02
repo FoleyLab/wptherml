99730f77989df26a93c77435599d5b462b64ddb6
FoleyLab <foleyj10@wpunj.edu>
2021-06-02 11:35:02 -0400

    added basic def __init__() to wpspec.py

--- wpspec/wpspec.py
+++ wpspec/wpspec.py
@@ -5,30 +5,57 @@ A python package for modeling light-matter interactions!
 Handles the primary functions
 """
 
+import numpy as np
+class wpspec:
+    
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+    def __init__(self, args):
+        """
+        Initilializer for the wpspec class
 
-    Replace this function and doc string for your own project
+        Will decide what structure we are dealing with and what we want to compute!
 
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+        Parameters
+        ----------
+        args : dictionary containing keywords and values that determine what structure we are modeling
+               and what we want to calculate
 
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
+        Returns
+        -------
+        None.
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+        Default values of attributes for first pass:
+        self.number_of_wavelengths : 400
+        self.wavelength_array[:]  : np.linspace(400e-9,800e-9,self.number_of_wavelengths) (array of wavelengths between 400 and 800 nm) 
+        self.number_of_layers : 3
+        self.refractive_index_array[:] : np.array([1+0j, 1.5 + 0j, 1+0j]) (air, glass, air)
+        self.thickness_array[:] : np.array([0, 100e-9, 0]) (infinite, 100 nm, infinite)
+        """
+        self.number_of_wavelengths = 400
+        self.wavelength_array = np.linspace(400e-9, 800e-9, self.number_of_wavelengths)
+        self.number_of_layers = 3
+        self.refractive_index_array = np.array([1.0+0j, 1.5+0j, 1.0+0j])
+        self.thickness_array = np.array([0, 100e-9, 0])
+    
 
+    def canvas(self):
+        """
+        will print values of attributes!
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        """
+        
+        print("Number of wavelengths", self.number_of_wavelengths)
+        print("number of layers",self.number_of_layers)
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+#if __name__ == "__main__":
+#    # Do something if this file is invoked on its own
+
+test_ml = wpspec([])
+print(test_ml.canvas())
