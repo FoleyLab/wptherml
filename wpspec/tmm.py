c1cfa2d3a5328d1829be4db0757c94d8e7f39088
FoleyLab <foleyj10@wpunj.edu>
2021-06-02 10:36:57 -0400

    created skeleton file for tmm.py

---
+++ wpspec/tmm.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def tmm():
+    """
+    tmm - a function for solving the transfer matrix equations
+    
+    Parameters
+    ----------
+    None
+
+    Returns 
+    -------
+
+    Returns 1
+    """
+    return 1
