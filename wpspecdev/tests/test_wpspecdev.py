532889d7a725652dc7dc03d0a9a8a3992b69a54f
jonmct123 <jonathan.mctague@gmail.com>
2021-06-11 12:28:22 -0400

    Prior to pulling James&Figen's fix

--- wpspecdev/tests/test_wpspecdev.py
+++ wpspecdev/tests/test_wpspecdev.py
@@ -31,15 +31,30 @@ def test_compute_mie_coefficients():
     #mt = sf.spectrum_factory('Mie', r)
     #mt._compute_mie_coeffients(ri, mu, x)
 
-    expected_an = np.array([1.51561398e-07-3.89308843e-04j,
-                            1.20867988e-13-3.47660737e-07j,
-                            2.12941403e-20-1.45925119e-10j,
-                            1.23296734e-27-3.51136346e-14j,
-                            2.97642974e-35-5.45566654e-18j, 
-                            3.49894198e-43-5.91518553e-22j,
-                            2.23336769e-51-4.72585198e-26j])
+    expected_an = np.array([1.09393760e-01-3.12132609e-01j, 9.58682444e-04-3.09477523e-02j,
+ 1.63054752e-06-1.27692790e-03j, 9.66113753e-10-3.10823704e-05j,
+ 2.39854703e-13-4.89749633e-07j, 2.88856083e-17-5.37453331e-09j,
+ 1.88087968e-21-4.33691097e-11j])
 
-    assert 1==1
+    expected_bn = np.array([8.51059716e-03-9.18594954e-02j, 1.35466949e-05-3.68055857e-03j,
+ 8.15604142e-09-9.03108042e-05j, 2.07821108e-12-1.44160018e-06j,
+ 2.56048144e-16-1.60015044e-08j, 1.69880890e-20-1.30338363e-10j,
+ 6.58730141e-25-8.11621920e-13j] )
+    
+    expected_cn = np.array([1.02133636+9.46247560e-02j, 0.5558344 +2.04580879e-03j,
+ 0.34394533+3.10619798e-05j, 0.22121405+3.18902215e-07j,
+ 0.14432436+2.30940695e-09j, 0.09482464+1.23592890e-11j,
+ 0.0625545 +5.07706007e-14j])
+    expected_dn = np.array([0.94943955+3.32752039e-01j, 0.57237172+1.77306162e-02j,
+ 0.33722182+4.30608658e-04j, 0.21300494+6.62069833e-06j,
+ 0.13775492+6.74654196e-08j, 0.09001634+4.83795816e-10j,
+ 0.05915672+2.56557430e-12j] )
+    check_lst = [expected_an, expected_bn, expected_cn, expected_dn]
+    #for i in check_lst:
+    result = mietest._compute_mie_coeffients(mietest._relative_refractive_index_array, mietest._relative_permeability, mietest._size_factor_array)
+    print(result[0],'\n'*3)
+    assert np.allclose(result[0], expected_an, 1e-1)
+        
     
 def test_compute_s_jn():
     """ tests private method in MieDriver _compute_s_jn(n, z) """
