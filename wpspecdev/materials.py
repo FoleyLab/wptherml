ef24eb0f898a6b1667b5c33691438bf6ba695a59
FoleyLab <foleyj10@wpunj.edu>
2021-06-14 14:53:34 -0400

    set up some files for gmmt

---
+++ wpspecdev/materials.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+
+class Materials():
+    """ Compute the absorption, scattering, and extinction spectra of a sphere using Mie theory
+    
+    """
+    def SiO2(self):
+        return 1.5
+        
+        
+        
+
