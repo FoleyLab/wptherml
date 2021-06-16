af7f5c649254c97b08a865490c76547679cfb94e
FoleyLab <jayfoleyiv@gmail.com>
2021-06-16 12:31:33 -0400

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -9,5 +9,3 @@ thickness = 100e-9
 #print(mt._refractive_index_array[:,1])
 materials_test = wpspecdev.Materials()
 
-materials_test._create_test_multilayer(central_wavelength=636e-9)
-print(materials_test.wavelength_array)
