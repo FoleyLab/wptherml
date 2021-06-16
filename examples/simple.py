566721e15c23c03cbb068e0e7985ca2931802abe
FoleyLab <foleyj10@wpunj.edu>
2021-06-16 12:02:03 -0400

    added requested materials, plus references and unit tests

--- examples/simple.py
+++ examples/simple.py
@@ -3,7 +3,11 @@ import numpy as np
 
 thickness = 100e-9
 
-sf = wpspecdev.SpectrumFactory()
-mt = sf.spectrum_factory('Tmm', thickness)
-#mt.material_sio2(1)
-#print(mt._medium_refractive_index)
+#sf = wpspecdev.SpectrumFactory()
+#mt = sf.spectrum_factory('Tmm', thickness)
+#mt.material_TiO2(1)
+#print(mt._refractive_index_array[:,1])
+materials_test = wpspecdev.Materials()
+
+materials_test._create_test_multilayer(central_wavelength=636e-9)
+print(materials_test.wavelength_array)
--- wpspecdev/materials.py
+++ wpspecdev/materials.py
@@ -8,14 +8,28 @@ class Materials():
     """ Compute the absorption, scattering, and extinction spectra of a sphere using Mie theory
     
     """
+    def _create_test_multilayer(self, central_wavelength):
+        """
+        Simple method to create a 3-entry array of wavelengths as follows:
+        [central_wavelength-1e-9 m, central_wavelength m, central_wavelength+1e-9 m]
+        and dummy _refractive_index_array that can be filled in 
+        with actual materials at the wavelength arrays.
+        This is simply meant to enable unit testing for desired wavelengths of the 
+        various materials methods
+        """
+        self.wavelength_array = np.array([central_wavelength-1e-9, central_wavelength, central_wavelength+1e-9])
+        self.number_of_wavelengths = 3 
+        self.number_of_layers = 3
+        self._refractive_index_array = np.reshape(np.tile(np.array([1+0j, 1+0j, 1+0j]), self.number_of_wavelengths), 
+                                                  (self.number_of_wavelengths, self.number_of_layers)) 
 
-    def material_air(self, layer_number):
+    def material_Air(self, layer_number):
         self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 1.0
 
-    def material_sio2(self, layer_number):
+    def material_SiO2(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
             # get path to the sio2 data file
-            file_path = path + 'data/sio2_ir.txt'
+            file_path = path + 'data/SiO2_ir.txt'
             # now read sio2 data into a numpy array
             file_data = np.loadtxt(file_path)
             # file_path[:,0] -> wavelengths in meters
@@ -25,12 +39,49 @@ class Materials():
             k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
 
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)
-        #print()
-    def material_tio2(self, layer_number):
+
+    def material_TiO2(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
             self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the tio2 data file
+            file_path = path + 'data/TiO2_Siefke.txt'
+            # now read TiO2 data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
 
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)
+
+    def material_Ta2O5(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the tio2 data file
+            file_path = path + 'data/Ta2O5_Bright.txt'
+            # now read TiO2 data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)
         
-        
-        
+    def material_TiN(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the tio2 data file
+            file_path = path + 'data/TiN_ellipsometry_data.txt'
+            # now read TiO2 data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
 
