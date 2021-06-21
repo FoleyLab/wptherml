587b916153236735252951c32f6c1c58d18cd283
Suchanekf <suchanekf@student.wpunj.edu>
2021-06-20 23:36:48 -0400

    refractive indexes for various materials

--- wpspecdev/materials.py
+++ wpspecdev/materials.py
@@ -85,3 +85,63 @@ class Materials():
 
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
 
+    def material_Al(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the Al data file
+            file_path = path + 'data/Al.txt'
+            # now read Al data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
+    
+    
+    def material_W(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the W data file
+            file_path = path + 'data/W.txt'
+            # now read W data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
+
+    def material_AlN(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the AlN data file
+            file_path = path + 'data/AlN.txt'
+            # now read AlN data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)   
+
+    def material_HfO2(self, layer_number):
+        if layer_number>0 and layer_number<(self.number_of_layers-1):
+            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
+            # get path to the HfO2 data file
+            file_path = path + 'data/HfO2.txt'
+            # now read AlN data into a numpy array
+            file_data = np.loadtxt(file_path)
+            # file_path[:,0] -> wavelengths in meters
+            # file_path[:,1] -> real part of the refractive index
+            # file_path[:,2] -> imaginary part of the refractive index
+            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
+            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
+
+            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)   
