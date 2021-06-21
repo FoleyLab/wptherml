3802852d2ddcfc1809312f4a04f63619b15e1c64
FoleyLab <jayfoleyiv@gmail.com>
2021-06-21 14:15:09 -0400

    Update materials.py

--- wpspecdev/materials.py
+++ wpspecdev/materials.py
@@ -42,7 +42,6 @@ class Materials():
 
     def material_TiO2(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the tio2 data file
             file_path = path + 'data/TiO2_Siefke.txt'
             # now read TiO2 data into a numpy array
@@ -57,7 +56,6 @@ class Materials():
 
     def material_Ta2O5(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the tio2 data file
             file_path = path + 'data/Ta2O5_Bright.txt'
             # now read TiO2 data into a numpy array
@@ -72,7 +70,6 @@ class Materials():
         
     def material_TiN(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the tio2 data file
             file_path = path + 'data/TiN_ellipsometry_data.txt'
             # now read TiO2 data into a numpy array
@@ -87,7 +84,6 @@ class Materials():
 
     def material_Al(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the Al data file
             file_path = path + 'data/Al.txt'
             # now read Al data into a numpy array
@@ -100,29 +96,8 @@ class Materials():
 
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
 
-<<<<<<< Updated upstream
-    def material_Al(self, layer_number):
-        if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
-            # get path to the Al data file
-            file_path = path + 'data/Al.txt'
-            # now read Al data into a numpy array
-            file_data = np.loadtxt(file_path)
-            # file_path[:,0] -> wavelengths in meters
-            # file_path[:,1] -> real part of the refractive index
-            # file_path[:,2] -> imaginary part of the refractive index
-            n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
-            k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
-
-            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
-    
-    
-=======
-
->>>>>>> Stashed changes
     def material_W(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the W data file
             file_path = path + 'data/W.txt'
             # now read W data into a numpy array
@@ -135,13 +110,8 @@ class Materials():
 
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)      
 
-<<<<<<< Updated upstream
-=======
-    
->>>>>>> Stashed changes
     def material_AlN(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
-            self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the AlN data file
             file_path = path + 'data/AlN.txt'
             # now read AlN data into a numpy array
@@ -151,35 +121,18 @@ class Materials():
             # file_path[:,2] -> imaginary part of the refractive index
             n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
             k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
-
-<<<<<<< Updated upstream
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)   
-=======
-            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array) 
->>>>>>> Stashed changes
 
     def material_HfO2(self, layer_number):
         if layer_number>0 and layer_number<(self.number_of_layers-1):
             self._refractive_index_array[:,layer_number] = np.ones(len(self.wavelength_array), dtype=complex) * 2.4
             # get path to the HfO2 data file
             file_path = path + 'data/HfO2.txt'
-<<<<<<< Updated upstream
-            # now read AlN data into a numpy array
-=======
-            # now read HfO2 data into a numpy array
->>>>>>> Stashed changes
             file_data = np.loadtxt(file_path)
             # file_path[:,0] -> wavelengths in meters
             # file_path[:,1] -> real part of the refractive index
             # file_path[:,2] -> imaginary part of the refractive index
             n_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,1], k=1)
             k_spline = InterpolatedUnivariateSpline(file_data[:,0], file_data[:,2], k=1)
-
-<<<<<<< Updated upstream
             self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array)   
-=======
-            self._refractive_index_array[:,layer_number] = n_spline(self.wavelength_array) + 1j * k_spline(self.wavelength_array) 
 
-    
-
->>>>>>> Stashed changes
