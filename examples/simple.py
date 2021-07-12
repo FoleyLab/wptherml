4d3d867f9c73f00dcadc6f08920fa743af71f664
FoleyLab <foleyj10@wpunj.edu>
2021-07-12 14:47:50 -0400

    can compute transfer matrix now

--- examples/simple.py
+++ examples/simple.py
@@ -4,10 +4,12 @@ from matplotlib import pyplot as plt
 
 thickness = 100e-9
 userargs = {
-    'radius': 170e-9,
-    'wavelength_list': [400e-9, 9000e-9, 500]
+    'wavelength_list': [450e-9, 9050e-9, 10]
 }
 sf = wpspecdev.SpectrumFactory()
-mt = sf.spectrum_factory('Mie', userargs)
-mt.material_Ta2O5(1)
+mt = sf.spectrum_factory('Tmm', userargs)
+
+mt.compute_spectrum()
+
+
 
