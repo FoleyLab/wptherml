40e036b7f941c60c69378f812ec5c203ac1e0eb2
FoleyLab <foleyj10@wpunj.edu>
2021-07-13 20:55:19 -0400

    first pass at integrating materials class with TmmDriver

--- examples/simple.py
+++ examples/simple.py
@@ -4,16 +4,23 @@ from matplotlib import pyplot as plt
 
 thickness = 100e-9
 userargs = {
-    'wavelength_list': [400e-9, 800e-9, 10]
+    'wavelength_list': [400e-9, 3000e-9, 500],
+    'material_list': ["Air", "TiO2", "SiO2", "Air"],
+    'thickness_list': [0, 200e-9, 100e-9, 0]
 }
 sf = wpspecdev.SpectrumFactory()
 mt = sf.spectrum_factory('Tmm', userargs)
+mt.set_refractive_indicex_array()
+
+
 
 
 
 # before compute spectrum, mt.q_ext is not define
 mt.compute_spectrum()
-print(mt.reflectivity_array)
+
+plt.plot(mt.wavelength_array, mt.reflectivity_array)
+plt.show()
 
 
 
