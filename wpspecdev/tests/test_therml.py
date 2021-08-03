df04cd15d33cb5e5a49a73f1a82b318cfab43499
FoleyLab <foleyj10@wpunj.edu>
2021-08-03 11:55:47 -0400

    ran black to fix formatting compliance issues with all python files

--- wpspecdev/tests/test_therml.py
+++ wpspecdev/tests/test_therml.py
@@ -10,23 +10,22 @@ import sys
 
 
 # define basic structure at 1500 K
-test_args = {  
-'wavelength_list': [100e-9, 30000e-9, 10000],  
-'material_list': ["Air", "W", "Air"],
-'thickness_list': [0, 800e-9, 0],
-'temperature' : 1500, 
-'therml' : True
-}  
+test_args = {
+    "wavelength_list": [100e-9, 30000e-9, 10000],
+    "material_list": ["Air", "W", "Air"],
+    "thickness_list": [0, 800e-9, 0],
+    "temperature": 1500,
+    "therml": True,
+}
 
-sf = wpspecdev.SpectrumFactory()  
-test = sf.spectrum_factory('Tmm', test_args)
-
+sf = wpspecdev.SpectrumFactory()
+test = sf.spectrum_factory("Tmm", test_args)
 
 
 def test_compute_power_density():
-    """ will test _compute_power_density method to 
-        see if the power density computed by integration
-        of the blackbody spectrum is close to the prediction
-        by the Stefan-Boltzmann law, where the latter should be exact
-        """
+    """will test _compute_power_density method to
+    see if the power density computed by integration
+    of the blackbody spectrum is close to the prediction
+    by the Stefan-Boltzmann law, where the latter should be exact
+    """
     assert np.isclose(test.blackbody_power_density, test.stefan_boltzmann_law, 1e-2)
