2b2cd2d59061c020d7df629ac7e4313dfc64bf90
FoleyLab <jayfoleyiv@gmail.com>
2022-03-26 19:03:30 -0400

    added methods to add and remove layers to multilayer stacks along with unit tests

--- examples/simple_therml.py
+++ examples/simple_therml.py
@@ -4,7 +4,7 @@ import numpy as np
 
 test_args = {
     "wavelength_list": [300e-9, 6000e-9, 1000],
-    "Material_List": ["Air", "Al2O3", "SiO2", "TiO2", "SiO2", "Al2O3", "W", "Air"],
+    "Material_List": ["Air", "Al2O3", "SiO2_ir.txt", "TiO2", "Ag_JC.txt", "Al2O3", "W", "Air"],
     "Thickness_List": [0, 20e-9, 255e-9, 150e-9, 255e-9, 10e-9, 900e-9, 0],
     "temperature": 1700,
     "therml": True
