7e1c2f4460b6a61828862aa5e4bf1cd74e220859
FoleyLab <jayfoleyiv@gmail.com>
2023-02-02 16:50:38 -0500

    started working on functionality to plot exciton wavefunctions
    Needs refinement and better documentation

--- wptherml/exciton.py
+++ wptherml/exciton.py
@@ -118,6 +118,9 @@ class ExcitonDriver(SpectrumDriver):
                 # <== call _compute_H0_element and store value -> H0
                 # <== call _compute_dipole_dipole_coupling and store value -> V
                 # <== assign H0 + V to appropriate element of self.exciton_hamiltonian
+                pass
+
+        pass
         
 
     def compute_spectrum(self):
@@ -134,4 +137,56 @@ class ExcitonDriver(SpectrumDriver):
 
         """
         pass
+
+    def compute_exciton_wavefunction_site_basis(self):
+        """
+        Will compute the single-exciton wavefunctions (approximated as Gaussians) for each site.  
+        
+        ***Note:***  This is probably not the best model... better model might be a Slater function, but 
+        we can fix that later. Also right now the width is somewhat arbitrary.  In reality, the exciton
+        has a Bohr radius that will determine the width.  We will update this later as well! 
+
+        Arguments
+        ---------
+        n : int
+            the site index offset from python index by +1
+
+        Attributes
+        ----------
+        x : 1 x _len numpy array of floats
+            the spatial grid that we will evaluate the exciton wavefunction on
+
+        phi : _len x number_of_monomer numpy array of floats
+            the single-exciton wavefunctions for each cite
+
+        """
+        # get distance between sites
+        _dx = self.coords[0, 1] - self.coords[0, 0]
+        
+        # full-width at half-max based on distance between sites
+        _fwhm = _dx / 2
+
+        # width parameter of the Gaussian
+        _c = _fwhm / 2.35482
+
+        # normalization of Gaussian
+        _a = 1 / (_c * np.sqrt(2 * np.pi))
+
+        # create grid of x values spanning all sites in the system
+        # get largest site index
+        _N_max = self.number_of_monomers - 1
+        # get x-value associated with largest site index 
+        _x_max = self.coords[0,_N_max]
+        # create the x-grid from 0 to _x_max
+        _len = 500
+        self.x = np.linspace(-_dx, _x_max, _len)
+
+        self.phi = np.zeros((_len, self.number_of_monomers ))
+
+        for n in range(self.number_of_monomers):
+            _x_n = self.coords[0, n]
+            self.phi[:, n] = _a * np.exp(- (self.x - _x_n) ** 2 / (2 * _c ** 2 ) )
+
+
+
 
