e1358c867dbf34786b8d3d5131facc01dd7708c7
lanetolley <113054686+lanetolley@users.noreply.github.com>
2023-06-29 10:42:26 -0400

    msd

--- wptherml/exciton.py
+++ wptherml/exciton.py
@@ -388,6 +388,7 @@ class ExcitonDriver(SpectrumDriver):
         self.density_matrix = np.copy(
             _d0 + 1 / 6 * (k_1 + 2 * k_2 + 2 * k_3 + k_4) * dt
         )
+        return self.density_matrix
 
 def _2D_rk_exciton(self, dt):
         """Function that will take c(t0) and H and return c(t0 + dt)
