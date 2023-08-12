29b262d6728aa7c6caf23841a6d0999020347ad3
FoleyLab <jayfoleyiv@gmail.com>
2023-08-12 12:48:10 -0400

    renamed optimize and super_func methods

--- wptherml/optdriver.py
+++ wptherml/optdriver.py
@@ -102,9 +102,9 @@ class OptDriver(TmmDriver):
 
 
 
-    def optimize(self):
+    def optimize_bfgs(self):
         """"
-        Method to wrap the optimizer
+        Method to wrap the l-bfgs-b optimizer
         """
         # initialize x array
         x_start = self.thickness_array[1:self.number_of_layers-1] * 1e9
@@ -115,20 +115,22 @@ class OptDriver(TmmDriver):
         # rewrite the bounds in the way required by L-BFGS-B
         bfgs_bounds = [(low, high) for low, high in zip(bfgs_xmin, bfgs_xmax)]
 
-        fom_start, grad_start = self.super_func(x_start)
+        fom_start, grad_start = self.compute_fom_and_gradient_from_thickness_array(x_start)
         print(F' Initial FOM is {fom_start}')
         print(F' Initial Gradient is {grad_start}')
 
-        ret = minimize(self.super_func, x_start, method="L-BFGS-B", jac=True, bounds=bfgs_bounds)
+        ret = minimize(self.compute_fom_and_gradient_from_thickness_array, x_start, method="L-BFGS-B", jac=True, bounds=bfgs_bounds)
         print(ret.x)
 
 
-    def super_func(self, x0):
+    def compute_fom_and_gradient_from_thickness_array(self, x0):
         """"
         Method to update the thickness array, the relevant spectra, objective function, and (if supported)
         the gradient
         """
-        print(" Entering SuperFunc ")
+        print(" Computing figure of merit and graident from thickness array ", x0)
+
+        # need to add options to use other figures of merit
         self.thickness_array[1:self.number_of_layers-1] = x0 * 1e-9
         self.compute_spectrum()
         self.compute_selective_mirror_fom()
@@ -148,9 +150,6 @@ class OptDriver(TmmDriver):
         print(F' Current FOM is {fom}')
         print(F' Current Gradient is {grad}')
         return fom, grad
-
-
-
 
 
     class _qubo_thickness_and_alloy_optimization:
